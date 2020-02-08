c5a4a7ef9667ac8010fea283e5254d2518f40faa
edwardtanzhao <edwardtanzhao@gmail.com>
2020-02-08 14:34:38 -0500

    Added in the basics
    Need to add the FSM now.

--- Project 3/cache.vhd
+++ Project 3/cache.vhd
@@ -31,8 +31,37 @@ architecture arch of cache is
 
 -- declare signals here
 
+	--address struct: 25 bits tag, 5 bits index, 2 bits offset
+	type state_type is (start, r, w, r_memread, r_memwrite, r_memwait, w_memwrite);
+	signal current_state : state_type;
+	signal next_state : state_type; 
+
+	--Cache: 1 bit for valid, 1 bit for dirty, 25 bits for tag, 128 bit blocks (data) 
+	--4096/128 = 32
+	type cache_type is array (0 to 31) of std_logic_vector (154 downto 0);
+	signal cache2 : cache_type;
+
 begin
 
 -- make circuits here
+
+	--process to reset or continue
+	process(clock, reset)
+		begin
+		--if reset signal was sent, tell FSM to go back to start
+			if reset = '1' then
+				state <= start;
+		--if clock signal is 1 and just recently changed to 1
+			elsif (clock = '1' and clock'event) then
+				state <= next_state;
+			end if;
+	end process
+
+	--process to know when to read, write, evict or overwrite, the main FSM
+	process (s_read, s_write, m_waitrequest, state)
+
+		begin
+
+		end process;
 
 end arch;
