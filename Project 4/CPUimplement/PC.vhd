45e2c47f2303717886b5f0b179782ea60e72b671
edwardtanzhao <edwardtanzhao@gmail.com>
2020-03-11 20:36:23 -0400

    Update PC.vhd

--- Project 4/CPUimplement/PC.vhd
+++ Project 4/CPUimplement/PC.vhd
@@ -4,28 +4,40 @@ use IEEE.numeric_std.all;
 
 entity pc is 
 port(
-	clk : in std_logic;
+	clock : in std_logic;
 	reset : in std_logic;
+	enable : in std_logic;
+	pc_sel : in std_logic;
 	pcInput : in std_logic_vector(31 downto 0);
-	pcOutput : out std_logic_vector(31 downto 0) := x"00000000");
+	pcOutput : out std_logic_vector(31 downto 0));
+	--next_pc_out : out std_logic_vector(31 downto 0));
 end pc;
 
-architecture pc_archecture of pc is
+architecture pc_architecture of pc is
+	signal count : std_logic_vector(31 downto 0) := "00000000000000000000000000000000";
 
 begin 
 
-process (clk, reset)
+process (clock, reset, enable)
 begin
 -- counter starts at zero
 -- change output according to the input
 -- if the reset is 1 then reset to zero
 
 	if(reset = '1') then
-		pcOutput <= x"00000000";
-	elsif (clk'event and clk = '1') then 
-		pcOutput <= pcInput;
+		count <= "00000000000000000000000000000000";
+	elsif rising_edge(clock) then 
+		if (enable = '1') then
+			if (pc_sel = '1') then
+				count <= pcInput;
+			else
+				count <= std_logic_vector(to_unsigned(to_integer(unsigned(count)) + 4, 32));
+			end if;
+		end if;
 	end if;
 
-end prccess;
+end process;
+
+pcOutput <= count;
 
 end pc_architecture;
